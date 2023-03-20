1e10a293098fa8b48ac1ef1faa047018a5f35647
Gaetan Jonathan BAKARY <gaetan.s118@gmail.com>
2023-03-20 02:44:02 +0000

    Try fixing comment not change when change vot

--- controllers/voting.py
+++ controllers/voting.py
@@ -85,7 +85,7 @@ def comment_vote(sender_id, yes, participant_id, update=False, **ext):
         )
     else:
         voter = Voter.from_fb_id(sender_id)
-        vote = Vote(voter, participant)
+        vote = Vote(voter, participant, None)
         if update:
             vote.refresh()
             vote.change_vote(participant)
